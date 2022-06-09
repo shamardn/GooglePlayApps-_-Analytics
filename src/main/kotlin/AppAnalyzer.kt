6600f2ed525dd5e9ea14c51112d0d6c8be6a1828
A10XY <86065368+A10XY@users.noreply.github.com>
2022-06-10 01:16:44 +0300

    fix some issues

--- src/main/kotlin/AppAnalyzer.kt
+++ src/main/kotlin/AppAnalyzer.kt
@@ -2,7 +2,7 @@ import model.App
 
 class AppAnalyzer {
 
-    fun getNumberOfAppsDevelopedBySomeCompany(appsList: MutableList<App>, companyName: String) =if(appsList.isEmpty()) null else appsList.filter { it.company.contains(companyName) }.size
+    fun getNumberOfAppsDevelopedBySomeCompany(appsList: MutableList<App>, companyName: String) = if(appsList.isEmpty()) 0 else appsList.filter { it.company.contains(companyName) }.size
 
     fun medicalAppsPercentage(appsList: MutableList<App>, category: String): Double {
         if(appsList.isEmpty()) return 0.0
@@ -13,7 +13,7 @@ class AppAnalyzer {
         return String.format("%.2f", counter.toDouble() / appsList.size * 100).toDouble()
     }
 
-    fun getOldestApp(appsList: List<App>) =if (appsList.isEmpty()) null else appsList.sortedBy { it.updated  }.subList(0,1)
+    fun getOldestApp(appsList: List<App>) =if (appsList.isEmpty()) null else appsList.sortedBy { it.updated  }.subList(0,1)[0]
 
     fun percentageOfAndroid9AndUp(appsList: List<App>,androidRequired: Double): Double{
         if(appsList.isEmpty()) return 0.0
@@ -24,9 +24,9 @@ class AppAnalyzer {
         return String.format("%.2f", counter.toDouble() / appsList.size * 100).toDouble()
     }
 
-    fun getLargest10Apps(appList: List<App>) =if (appList.isEmpty()) null else appList.sortedByDescending { it.size }.subList(0, 10)
+    fun getLargest10Apps(appList: List<App>) = if (appList.isEmpty()) null else appList.sortedByDescending { it.size }.subList(0, 10)
 
-    fun getTop10InstalledApps(appsList: MutableList<App>)= if (appsList.isEmpty()) null else appsList.sortedByDescending { it.installs }.subList(0, 10)
+    fun getTop10InstalledApps(appsList: MutableList<App>) = if (appsList.isEmpty()) null else appsList.sortedByDescending { it.installs }.subList(0, 10)
 
     fun getLargestAppSizeDevelopedBySomeCompany(appsList: MutableList<App>, companyName: String)= if (appsList.isEmpty()) null else appsList.filter { it.company.contains(companyName)}.maxByOrNull { it.size }
 
